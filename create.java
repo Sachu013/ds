c27e6226b2458aab77b1c444488cd999d111a2cb
Sachu013 <sachinsuresh.1311@gmail.com>
2025-07-02 12:09:07 +0530

    renamed

---
+++ create.java
@@ -0,0 +1,24 @@
+
+public class create 
+{
+    class node
+    {
+        int data;
+        node next;
+
+        node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+    node head;
+
+    
+    
+}
+
+public static void main(String[] args)
+{
+    create list = new create();
+}
